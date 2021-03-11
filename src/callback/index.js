315f31d48ebd3317f7de6977ed8412d5e2633e34
Juan Vilchez <juanvilchez2002@gmail.com>
2021-03-10 23:14:56 -0500

    Definición Estructura Callback

---
+++ src/callback/index.js
@@ -0,0 +1,36 @@
+//-----
+//un callback es una función que se pasa como parametro a otra función
+//-----
+//creando una función que recibe una función como parametro
+//función suma
+function sum(num1, num2){
+    return num1 + num2;
+}
+
+//funcion que recibe la funcion sum
+function calc(num1, num2, callback){
+    return callback(num1, num2);
+}
+
+//realizamos el llamado donde se pasa la función
+//sum como parametro
+console.log(calc(2,6,sum));
+
+//-----
+//esperar a algo que suceda con callback
+// función que se pasara el callback
+function date(callback){
+    console.log(new Date);
+    setTimeout(
+        function (){
+            let date = new Date;
+            callback(date);
+        }, 3000
+    );
+}
+//función que se pasara como parametro
+function printDate(dateNow){
+    console.log(dateNow);
+}
+//realizar la función
+date(printDate);
